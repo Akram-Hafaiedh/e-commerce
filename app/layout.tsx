afba9eb64e50c39ed4b29d209c7f65706f155dd9
Hafaiedh Akram <hfaiedhakram@gmail.com>
2025-10-15 11:33:53 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from './components/Navbar';
 import { CartProvider } from './context/CartContext';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Ecommerce Store',
@@ -21,6 +22,7 @@ export default function RootLayout({
           <main>
             {children}
           </main>
+          <Analytics />
         </CartProvider>
       </body>
     </html>
