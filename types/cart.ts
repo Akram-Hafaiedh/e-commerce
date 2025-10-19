3b44045d8a89cb60788d655e9ee9f2f742f35d16
Hafaiedh Akram <hfaiedhakram@gmail.com>
2025-10-19 17:19:52 +0100

    update for types

--- types/cart.ts
+++ types/cart.ts
@@ -1,6 +1,26 @@
+import { Order } from "./order";
 import { Product } from "./product";
 
 export interface CartItem {
     product: Product;
     quantity: number;
+}
+
+
+export interface CartContextType {
+    items: CartItem[];
+    addToCart: (product: Product, quantity?: number) => void;
+    removeFromCart: (productId: string) => void;
+    updateQuantity: (productId: string, quantity: number) => void;
+    clearCart: () => void;
+    getTotalItems: () => number;
+    getTotalPrice: () => number;
+    orders: Order[];
+    addOrder: (order: Omit<Order, 'id'>) => void;
+}
+
+
+export interface CartDropdownProps {
+    isOpen: boolean;
+    onClose: () => void;
 }
